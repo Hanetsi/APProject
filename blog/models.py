24b3a078d9b891fcdd4e0ffde6d1fd42abc93ca9
Kiusala <eetu.m.virtanen@gmail.com>
2021-11-13 21:07:24 +0200

    # Blog model # BlogPost model # Linked model to admin site

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,24 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Blog(models.Model):
+    name = models.CharField(max_length=100,unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_added = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name + " | " +  str(self.date_added.date())
+
+class BlogPost(models.Model):
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    """Unique parmeter makes sure there cannot be two blogs whit same name"""
+    blogpost_title = models.CharField(max_length=200, unique=True)
+    blogpost_content = models.CharField(max_length=2000)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_added = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+            return self.blogpost_title + " | " + str(self.date_modified.date())
